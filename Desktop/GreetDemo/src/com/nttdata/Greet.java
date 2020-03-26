dd25e186db3540cf214b71f625ca0b74f2612b1a
Dikshakeshri <dikshakeshri12@gmail.com>
2020-03-26 17:29:42 +0530

    this is second commit

---
+++ Desktop/GreetDemo/src/com/nttdata/Greet.java
@@ -0,0 +1,15 @@
+package com.nttdata;
+
+import java.util.Scanner;
+
+public class Greet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter name");
+String n=sc.nextLine();
+System.out.println("welocme to github :" +n);
+	}
+
+}
